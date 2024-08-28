04b000dfe4cc0758a9debc963f698fda5a6b00d6
Ruslan <ruslan.grudistov@gmail.com>
2024-08-28 16:47:13 +0300

    fix

--- task 2/01_lexer.py
+++ task 2/01_lexer.py
@@ -2,7 +2,7 @@ class LexicalError(Exception):
     pass
 
 
-class SyntaxError(Exception):
+class ParsingError(Exception):
     pass
 
 
@@ -15,7 +15,6 @@ class TokenType:
     LPAREN = "LPAREN"
     RPAREN = "RPAREN"
     EOF = "EOF"  
-
 
 class Token:
     def __init__(self, type, value):
@@ -33,6 +32,7 @@ class Lexer:
         self.current_char = self.text[self.pos]
 
     def advance(self):
+        """Перемещаем 'указатель' на следующий символ входного ряда"""
         self.pos += 1
         if self.pos > len(self.text) - 1:
             self.current_char = None  
@@ -40,10 +40,12 @@ class Lexer:
             self.current_char = self.text[self.pos]
 
     def skip_whitespace(self):
+        """Пропускаем пробельные символы."""
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
 
     def integer(self):
+        """Возвращаем целое число, собранное из последовательности цифр."""
         result = ""
         while self.current_char is not None and self.current_char.isdigit():
             result += self.current_char
@@ -51,6 +53,7 @@ class Lexer:
         return int(result)
 
     def get_next_token(self):
+        """Лексический анализатор, который разбивает входной ряд на токены."""
         while self.current_char is not None:
             if self.current_char.isspace():
                 self.skip_whitespace()
@@ -83,25 +86,138 @@ class Lexer:
                 self.advance()
                 return Token(TokenType.RPAREN, ")")
 
-            raise LexicalError("Помилка лексичного аналізу")
+            raise LexicalError("Ошибка лексического анализа")
 
         return Token(TokenType.EOF, None)
+
+
+class AST:
+    pass
+
+
+class BinOp(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+
+
+class Num(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+
+class Parser:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise ParsingError("Ошибка синтаксического анализа")
+
+    def eat(self, token_type):
+        """
+        Сравниваем текущий токен с ожидаемым токеном и, если они совпадают,
+        'поглощаем' его и переходим к следующему токену.
+        """
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+
+    def factor(self):
+        """Парсер для факторов (числа или выражения в скобках)."""
+        token = self.current_token
+        if token.type == TokenType.INTEGER:
+            self.eat(TokenType.INTEGER)
+            return Num(token)
+        elif token.type == TokenType.LPAREN:
+            self.eat(TokenType.LPAREN)
+            node = self.expr()
+            self.eat(TokenType.RPAREN)
+            return node
+
+    def term(self):
+        """Парсер для 'term' правил грамматики. Включает умножение и деление."""
+        node = self.factor()
+
+        while self.current_token.type in (TokenType.MUL, TokenType.DIV):
+            token = self.current_token
+            if token.type == TokenType.MUL:
+                self.eat(TokenType.MUL)
+            elif token.type == TokenType.DIV:
+                self.eat(TokenType.DIV)
+
+            node = BinOp(left=node, op=token, right=self.factor())
+
+        return node
+
+    def expr(self):
+        """Парсер для арифметических выражений. Включает сложение и вычитание."""
+        node = self.term()
+
+        while self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
+            token = self.current_token
+            if token.type == TokenType.PLUS:
+                self.eat(TokenType.PLUS)
+            elif token.type == TokenType.MINUS:
+                self.eat(TokenType.MINUS)
+
+            node = BinOp(left=node, op=token, right=self.term())
+
+        return node
+
+
+class Interpreter:
+    def __init__(self, parser):
+        self.parser = parser
+
+    def visit_BinOp(self, node):
+        if node.op.type == TokenType.PLUS:
+            return self.visit(node.left) + self.visit(node.right)
+        elif node.op.type == TokenType.MINUS:
+            return self.visit(node.left) - self.visit(node.right)
+        elif node.op.type == TokenType.MUL:
+            return self.visit(node.left) * self.visit(node.right)
+        elif node.op.type == TokenType.DIV:
+            right_value = self.visit(node.right)
+            if right_value == 0:
+                raise ZeroDivisionError("Деление на ноль недопустимо!")
+            return self.visit(node.left) / right_value
+
+    def visit_Num(self, node):
+        return node.value
+
+    def interpret(self):
+        tree = self.parser.expr()
+        return self.visit(tree)
+
+    def visit(self, node):
+        method_name = "visit_" + type(node).__name__
+        visitor = getattr(self, method_name, self.generic_visit)
+        return visitor(node)
+
+    def generic_visit(self, node):
+        raise Exception(f"Нет метода visit_{type(node).__name__}")
 
 
 def main():
     while True:
         try:
-            text = input('Введіть вираз (або "exit" для виходу): ')
+            text = input('Введите выражение (или "exit" для выхода): ')
             if text.lower() == "exit":
-                print("Вихід із програми.")
+                print("Выход из программы.")
                 break
             lexer = Lexer(text)
-            token = lexer.get_next_token()
-            while token.type != TokenType.EOF:
-                print(token)
-                token = lexer.get_next_token()
+            parser = Parser(lexer)
+            interpreter = Interpreter(parser)
+            result = interpreter.interpret()
+            print(result)
+        except ZeroDivisionError as e:
+            print(f"Ошибка: {e}")
         except Exception as e:
-            print(e)
+            print(f"Ошибка: {e}")
 
 
 if __name__ == "__main__":
